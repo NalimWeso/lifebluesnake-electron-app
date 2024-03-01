a4b22d0a6a62f2b2f6b9c5322bc6238d9ed9d4d1
Milan Wesołowski <milanweso@gmail.com>
2024-03-01 13:36:03 +0100

    Delete resources/app directory

--- resources/app/scripts/main.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e9827cd3526be8bb33254226f384c445a6de9c370ddf6a5fb28c483378db4379
-size 481
--- resources/app/scripts/saving.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:71576a0b1946204df7361655931ec1637398b7f972c648cd95f8741179b1dcbc
-size 315
